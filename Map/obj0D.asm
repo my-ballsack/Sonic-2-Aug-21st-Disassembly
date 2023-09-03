8335a53f70434d94cd98c0735ac3e4831f11a6fc
PikaNoob <87725268+PikaNoob@users.noreply.github.com>
2023-09-03 21:23:17 +0100

    separating more mapping files

---
+++ Map/obj0D.asm
@@ -0,0 +1,33 @@
+                dc.w    Offset_0x00F2BE-End_Panel_Mappings
+                dc.w    Offset_0x00F2D8-End_Panel_Mappings
+                dc.w    Offset_0x00F2F2-End_Panel_Mappings
+                dc.w    Offset_0x00F30C-End_Panel_Mappings
+                dc.w    Offset_0x00F31E-End_Panel_Mappings
+                dc.w    Offset_0x00F330-End_Panel_Mappings
+Offset_0x00F2BE:
+                dc.w    $0003
+                dc.l    $F00B0000, $0000FFE8
+                dc.l    $F00B000C, $00060000
+                dc.l    $10010050, $0028FFFC
+Offset_0x00F2D8:
+                dc.w    $0003
+                dc.l    $F00B0018, $000CFFE8
+                dc.l    $F00B0024, $00120000
+                dc.l    $10010050, $0028FFFC
+Offset_0x00F2F2:
+                dc.w    $0003
+                dc.l    $F00B0030, $0018FFE8
+                dc.l    $F00B0830, $08180000
+                dc.l    $10010050, $0028FFFC
+Offset_0x00F30C:
+                dc.w    $0002
+                dc.l    $F00F003C, $001EFFF0
+                dc.l    $10010050, $0028FFFC
+Offset_0x00F31E:
+                dc.w    $0002
+                dc.l    $F003004C, $0026FFFC
+                dc.l    $10010050, $0028FFFC
+Offset_0x00F330:
+                dc.w    $0002
+                dc.l    $F00F083C, $081EFFF0
+                dc.l    $10010050, $0028FFFC
---
+++ Map/obj79.asm
@@ -0,0 +1,21 @@
+                dc.w    Offset_0x014700-Lamp_Post_Mappings
+                dc.w    Offset_0x014722-Lamp_Post_Mappings
+                dc.w    Offset_0x014744-Lamp_Post_Mappings
+Offset_0x014700:
+                dc.w    $0004
+                dc.l    $E8012000, $2000FFF8
+                dc.l    $E8012800, $28000000
+                dc.l    $F8030006, $0003FFF8
+                dc.l    $F8030806, $08030000
+Offset_0x014722:
+                dc.w    $0004
+                dc.l    $E8010002, $0001FFF8
+                dc.l    $E8010802, $08010000
+                dc.l    $F8030006, $0003FFF8
+                dc.l    $F8030806, $08030000
+Offset_0x014744:
+                dc.w    $0004
+                dc.l    $E8012004, $2002FFF8
+                dc.l    $E8012804, $28020000
+                dc.l    $F8030006, $0003FFF8
+                dc.l    $F8030806, $08030000
