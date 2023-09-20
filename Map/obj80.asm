27e37455b4067c9fe8420190380b3cc92b098675
PikaNoob <87725268+PikaNoob@users.noreply.github.com>
2023-09-20 21:01:35 +0100

    will finish later

---
+++ Map/obj80.asm
@@ -0,0 +1,63 @@
+                dc.w    Offset_0x01EC22-Vines_Mappings
+                dc.w    Offset_0x01EC3C-Vines_Mappings
+                dc.w    Offset_0x01EC5E-Vines_Mappings
+                dc.w    Offset_0x01EC88-Vines_Mappings
+                dc.w    Offset_0x01ECBA-Vines_Mappings
+                dc.w    Offset_0x01ECF4-Vines_Mappings
+                dc.w    Offset_0x01ED36-Vines_Mappings
+Offset_0x01EC22:
+                dc.w    $0003
+                dc.l    $30030000, $0000FFFC
+                dc.l    $50030000, $0000FFFC
+                dc.l    $70090004, $0002FFF4
+Offset_0x01EC3C:
+                dc.w    $0004
+                dc.l    $10030000, $0000FFFC
+                dc.l    $30030000, $0000FFFC
+                dc.l    $50030000, $0000FFFC
+                dc.l    $70090004, $0002FFF4
+Offset_0x01EC5E:
+                dc.w    $0005
+                dc.l    $F0030000, $0000FFFC
+                dc.l    $10030000, $0000FFFC
+                dc.l    $30030000, $0000FFFC
+                dc.l    $50030000, $0000FFFC
+                dc.l    $70090004, $0002FFF4
+Offset_0x01EC88:
+                dc.w    $0006
+                dc.l    $D0030000, $0000FFFC
+                dc.l    $F0030000, $0000FFFC
+                dc.l    $10030000, $0000FFFC
+                dc.l    $30030000, $0000FFFC
+                dc.l    $50030000, $0000FFFC
+                dc.l    $70090004, $0002FFF4
+Offset_0x01ECBA:
+                dc.w    $0007
+                dc.l    $B0030000, $0000FFFC
+                dc.l    $D0030000, $0000FFFC
+                dc.l    $F0030000, $0000FFFC
+                dc.l    $10030000, $0000FFFC
+                dc.l    $30030000, $0000FFFC
+                dc.l    $50030000, $0000FFFC
+                dc.l    $70090004, $0002FFF4
+Offset_0x01ECF4:
+                dc.w    $0008
+                dc.l    $90030000, $0000FFFC
+                dc.l    $B0030000, $0000FFFC
+                dc.l    $D0030000, $0000FFFC
+                dc.l    $F0030000, $0000FFFC
+                dc.l    $10030000, $0000FFFC
+                dc.l    $30030000, $0000FFFC
+                dc.l    $50030000, $0000FFFC
+                dc.l    $70090004, $0002FFF4
+Offset_0x01ED36:
+                dc.w    $0009
+                dc.l    $80010002, $0001FFFC
+                dc.l    $90030000, $0000FFFC
+                dc.l    $B0030000, $0000FFFC
+                dc.l    $D0030000, $0000FFFC
+                dc.l    $F0030000, $0000FFFC
+                dc.l    $10030000, $0000FFFC
+                dc.l    $30030000, $0000FFFC
+                dc.l    $50030000, $0000FFFC
+                dc.l    $70090004, $0002FFF4 
