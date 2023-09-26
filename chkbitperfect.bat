5ac7396852401de65c57a868e456b2a23ac3e780
RepellantMold <repellantmold@outlook.com>
2023-09-25 17:39:14 -0700

    okay let me use redirection

--- chkbitperfect.bat
+++ chkbitperfect.bat
@@ -5,7 +5,7 @@ call build
 
 REM  // run fc against the original ROM
 echo -------------------------------------------------------------
-if exist s2built.bin ( fc /b s2built.bin sonic2alpha.bin
+if exist s2built.bin ( fc /b s2built.bin sonic2alpha.bin >NUL && exit 0 || exit 1
 ) else echo s2built.bin does not exist, probably due to an assembly error
 
 REM // if someone ran this from Windows Explorer, prevent the window from disappearing immediately
